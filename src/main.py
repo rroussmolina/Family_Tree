39c25b4de17a06cccb3473ebe5fc068de9662389
rroussmolina <rroussmolina@gmail.com>
2021-05-04 21:45:59 +0000

    API Family Tree

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, Member, Parent, Children
 #from models import Person
 
 app = Flask(__name__)
@@ -30,14 +30,25 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/all', methods=['GET'])
+def handle_all():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    return jsonify(Member.getAllMembers()), 200
 
-    return jsonify(response_body), 200
+@app.route('/member/<int:id>', methods=['GET'])
+def handle_member(id):
+    member = Member.query.get(id)
+    return jsonify(member.serialize()), 200
+
+
+
+
+
+
+
+
+
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
